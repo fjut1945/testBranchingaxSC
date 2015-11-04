11fdad7c3db82910af3ce3e1efec630b7406ddc6
fjut1945 <>
2015-11-04 13:57:50 +0100

    yyy

--- Tables/dbo.Product.sql
+++ Tables/dbo.Product.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[Product] (
 		[Price]           [smallmoney] NULL,
 		[Active]          [bit] NULL,
 		[asd]             [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[asds1]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[asds1]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[asds2]           [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Product]
